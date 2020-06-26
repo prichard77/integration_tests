b60580dfcc28f81c7c6c399c78020c0d025fa04a
PJ Richardson <prichard@redhat.com>
2020-06-25 20:27:09 -0400

    Remove marker

--- cfme/tests/infrastructure/test_host.py
+++ cfme/tests/infrastructure/test_host.py
@@ -473,8 +473,6 @@ def test_infrastructure_hosts_compare(appliance, setup_provider_min_hosts, provi
 
 
 @test_requirements.infra_hosts
-@pytest.mark.uncollectif(lambda provider: provider.one_of(RHEVMProvider),
-                        reason="RHV providers blocked for BZ 1805803 marked WONTFIX")
 @pytest.mark.parametrize("num_hosts", [2, 4])
 @pytest.mark.parametrize("hosts_collection", ["provider", "appliance"])
 @pytest.mark.parametrize('report_format', DownloadOptions, ids=[fmt.name for fmt in
